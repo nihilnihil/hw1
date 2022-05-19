e496f990ff3e25d3d3b5e1b827ff809fb071939e
Endre Beda <endre.beda@novaprospekt.at>
2022-05-19 14:50:14 +0200

    AdditionalClass added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        AdditionalClass additionalClass = new AdditionalClass();
+        additionalClass.status = 1;
         System.out.println("Hello world!");
     }
 }
